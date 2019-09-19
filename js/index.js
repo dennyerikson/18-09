1351090fa6ed957d5da1915239d643eb74f05779
dennyerikson <dennyeriks00on@gmail.com>
2019-09-18 21:40:27 -0300

    Projeto finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
 
 // calculo pitágoras
+function calc(){
+    var b = parseFloat(document.getElementById('b').value.replace(",","."));
+    var c = parseFloat(document.getElementById('c').value.replace(",","."));
+    var distancia =0;
+
+    distancia = Math.sqrt((b**2) + (c**2));
+
+    document.getElementById("distancia").innerHTML = parseFloat(distancia).toFixed(2)
+
+    if(distancia>0){
+        alert("Maior que 0");
+    }
+};
 
